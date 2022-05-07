68100f7a9d46a970a5acbfc7359cc2cebe57deba
umitcanagbulak <umitcanagbulak1@gmail.com>
2022-05-08 02:04:26 +0300

    Core layer added.

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T> where T : class, IEntity
+    {
+        //Standart entity operations.
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+        IEnumerable<T> GetList(Expression<Func<T, bool>>? expression = null);
+        Task<IEnumerable<T>> GetListAsync(Expression<Func<T, bool>>? expression = null);
+        T Get(Expression<Func<T, bool>> expression);
+        Task<T> GetAsync(Expression<Func<T, bool>> expression);
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+        IQueryable<T> Query();
+        int GetCount(Expression<Func<T, bool>>? expression = null);
+        Task<int> GetCountAsync(Expression<Func<T, bool>>? expression = null);
+        Task<int> Execute(FormattableString interpolatedQueryString);
+    }
+}
---
+++ Core/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class BaseEntity: BaseEntity<int> { }
+
+    public class BaseEntity<T> : IEntity
+    {
+        public virtual T Id { get; set; }
+    }
+}
